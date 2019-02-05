3bf39ff98e3bcc83a0fd95373c5ee8ae6f813d0f
Craz1k0ek <bram_kolkman@hotmail.com>
2019-02-05 23:22:13 +0100

    Created String utility file
    Added a boolean computed value to verify if a string is a hexString
    Added the ability to insert another string every n characters

---
+++ String+Ext.swift
@@ -0,0 +1,41 @@
+//
+//  String+Ext.swift
+//
+//  Created by Craz1k0ek on 05/02/2019.
+//
+
+import Foundation
+
+extension String {
+    
+    /// Indication if a `String` contains only hexadecimal characters.
+    var isHexString: Bool {
+        let hexCharacterSet = CharacterSet(charactersIn: "0123456789abcdefABCDEF")
+        return self.rangeOfCharacter(from: hexCharacterSet.inverted) == nil
+    }
+    
+}
+
+extension StringProtocol where Self: RangeReplaceableCollection {
+    private mutating func inserting(separator: String, every n: Int) {
+        indices.reversed().forEach {
+            if $0 != startIndex {
+                if distance(from: startIndex, to: $0) % n == 0 {
+                    insert(contentsOf: separator, at: $0)
+                }
+            }
+        }
+    }
+    
+    /// Insert a specific `String` after every `n` characters.
+    ///
+    /// - Parameters:
+    ///   - separator: The string to insert.
+    ///   - n: The amount of characters to place the separator after.
+    /// - Returns: The `String` separated every nth character by the given separator.
+    func insert(separator: String, every n: Int) -> Self {
+        var string = self
+        string.inserting(separator: separator, every: n)
+        return string
+    }
+}
