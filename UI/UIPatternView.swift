ff92e8d019e64ddf8b56bb99778bdc11d7ca7ff9
Craz1k0ek <bram_kolkman@hotmail.com>
2019-04-23 13:47:39 +0200

    Created UIPatternView file
    Added the pattern image as IBInspectable, so the pattern image can be added in storyboards and in code
    Added the pattern scale as IBInspectable, so it can be used in storyboards and in code

---
+++ UI/UIPatternView.swift
@@ -0,0 +1,36 @@
+//
+//  UIPatternView.swift
+//
+//  Created by Craz1k0ek on 23/04/2019.
+//
+
+import UIKit
+
+@IBDesignable class UIPatternView: UIView {
+    
+    /// The `UIImage` that should be used as a background pattern.
+    @IBInspectable public var patternImage: UIImage? = nil {
+        didSet {
+            backgroundColor = patternImage != nil ? UIColor(patternImage: patternImage!) : .clear
+        }
+    }
+    
+    /// The scale of the background image.
+    @IBInspectable public var patternScale: CGFloat = 1.0 {
+        didSet {
+            if patternImage == nil { return }
+            
+            let size = patternImage!.size
+            let scaled = CGSize(width: size.width * patternScale, height: size.height * patternScale)
+            
+            UIGraphicsBeginImageContextWithOptions(scaled, false, 0)
+            patternImage!.draw(in: CGRect(x: 0, y: 0, width: scaled.width, height: scaled.height))
+            
+            if let scaledImage = UIGraphicsGetImageFromCurrentImageContext() {
+                backgroundColor = UIColor(patternImage: scaledImage)
+            }
+            UIGraphicsEndImageContext()
+        }
+    }
+
+}
