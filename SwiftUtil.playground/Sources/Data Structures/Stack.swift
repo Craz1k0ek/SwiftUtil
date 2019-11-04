2b3698b0be2aa0d2107942231b55b369f232c3b1
Bram Kolkman <bramkolkman@Hydrogen.local>
2019-11-04 11:10:51 +0100

    STACK
    + Added a stack structure to the repository, representing a stack type.

---
+++ SwiftUtil.playground/Sources/Data Structures/Stack.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+/// The stack is an array with limited functionality. You can only append items to the `top` of the stack and remove elements from the `top`.
+public struct Stack<T> {
+    
+    /// The internal array that holds the data.
+    fileprivate var array = [T]()
+    
+    /// Boolean value indicating whether or not the stack is empty.
+    public var isEmpty: Bool {
+        array.isEmpty
+    }
+    
+    /// The number of items in the stack.
+    public var count: Int {
+        array.count
+    }
+    
+    /// The current element at the top of the stack.
+    /// - Note: This is just a peek to see what's at the top, this will not pop the top of the stack.
+    public var top: T? {
+        array.last
+    }
+    
+    /// Push a new element onto the stack.
+    /// - Parameter element: The element to push to the stack.
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    /// Pop the last item from the stack (when able).
+    public mutating func pop() -> T? {
+        array.popLast()
+    }
+    
+}
